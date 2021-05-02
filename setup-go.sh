15d414cf0642611eae90427ec0a37058889e2f4c
Akeem Philbert <akeem.philbert@wepala.com>
2021-05-02 13:58:14 -0400

    WEOS-1000 Made build CGO enabled
    * Added more libraries for cross compiling  https://dh1tw.de/2019/12/cross-compiling-golang-cgo-projects/

--- setup-go.sh
+++ setup-go.sh
@@ -24,6 +24,13 @@ apt update \
  && apt install -y \
     gcc \
     libblkid-dev \
+    gcc-aarch64-linux-gnu \
+    libc6-dev-arm64-cross \
+    gcc-multilib \
+    libc6-dev-i386 \
+    linux-libc-dev:i386 \
+    gcc-mingw-w64-x86-64 \
+    gcc-mingw-w64-i686 \
  && apt clean \
  && apt autoremove -y \
  && rm -rf /var/lib/apt/lists/*
